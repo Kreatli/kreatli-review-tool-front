dec63e69e18491d8e4895d1a081b9d126a22635c
wolkyura <yura02505@gmail.com>
2025-10-14 20:00:07 +0200

    Add buttons

--- src/components/layout/Storyblok/Section/Section.tsx
+++ src/components/layout/Storyblok/Section/Section.tsx
@@ -1,13 +1,14 @@
 import { storyblokEditable } from '@storyblok/react';
 import { SectionStoryblok } from '../../../../typings/storyblok';
-import { cn } from '@heroui/react';
+import { Button, cn } from '@heroui/react';
+import NextLink from 'next/link';
 
 interface Props {
   blok: SectionStoryblok;
 }
 
 export const Section = ({ blok }: Props) => {
-  const { title, text, image, orientation } = blok;
+  const { title, text, image, orientation, buttons } = blok;
 
   return (
     <div
@@ -20,6 +21,22 @@ export const Section = ({ blok }: Props) => {
       <div>
         <h2 className="text-4xl font-sans max-w-xl font-bold mb-4">{title}</h2>
         {text && <p className="text-lg text-foreground-500">{text}</p>}
+        {buttons && buttons?.length > 0 && (
+          <div className="flex flex-wrap gap-4 mt-4">
+            {buttons.map((button) => (
+              <Button
+                key={button._uid}
+                as={NextLink}
+                href={button.url.linktype === 'story' ? button.url.cached_url : button.url.url}
+                target={button.url.target}
+                className={cn({ 'bg-foreground text-content1': !button.variant || button.variant === 'solid' })}
+                variant={button.variant || 'solid'}
+              >
+                {button.label}
+              </Button>
+            ))}
+          </div>
+        )}
       </div>
       {image?.filename && (
         <div className="max-h-[500px] shadow-large overflow-hidden mt-4 rounded-medium outline outline-1 outline-offset-2 outline-foreground-300">
--- src/typings/storyblok.d.ts
+++ src/typings/storyblok.d.ts
@@ -1,5 +1,39 @@
 import {StoryblokStory} from 'storyblok-generate-ts'
 
+export type MultilinkStoryblok =
+  | {
+      id?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "story";
+      target?: "_self" | "_blank";
+      [k: string]: any;
+    }
+  | {
+      url?: string;
+      cached_url?: string;
+      anchor?: string;
+      linktype?: "asset" | "url";
+      target?: "_self" | "_blank";
+      [k: string]: any;
+    }
+  | {
+      email?: string;
+      linktype?: "email";
+      target?: "_self" | "_blank";
+      [k: string]: any;
+    };
+
+export interface ButtonStoryblok {
+  label: string;
+  url: Exclude<MultilinkStoryblok, {linktype?: "email"} | {linktype?: "asset"}>;
+  newTab: boolean;
+  variant?: "" | "solid" | "light" | "flat" | "bordered";
+  _uid: string;
+  component: "button";
+  [k: string]: any;
+}
+
 export interface PageStoryblok {
   readTime?: string;
   body: (SectionStoryblok | WysiwygStoryblok)[];
@@ -32,6 +66,7 @@ export interface SectionStoryblok {
   text?: string;
   image?: AssetStoryblok;
   orientation: "" | "vertical" | "horizontal";
+  buttons?: ButtonStoryblok[];
   _uid: string;
   component: "section";
   [k: string]: any;
