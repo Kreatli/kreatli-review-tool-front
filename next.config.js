f25892fefccbfc0ed17a894368c2eeb789a51e0e
wolkyura <yura02505@gmail.com>
2025-11-01 19:25:53 +0100

    Add new aws domain

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['res.cloudinary.com', 'kreatlimedia.s3.amazonaws.com'],
+    domains: ['res.cloudinary.com', 'kreatlimedia.s3.amazonaws.com', 'kreatliassets.s3.amazonaws.com'],
   },
   experimental: {
     scrollRestoration: true,
